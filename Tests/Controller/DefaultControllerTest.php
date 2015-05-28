5ea1cb738daee0ea7f5709a4374f472a4b049a12
Mykees <mykeespro@gmail.com>
2015-05-28 12:13:54 +0200

    update

--- Tests/Controller/DefaultControllerTest.php
+++ Tests/Controller/DefaultControllerTest.php
@@ -1,12 +1,34 @@
 <?php
 
-namespace Mvc\BlogBundle\Tests\Controller;
+namespace Mykees\MediaBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    protected $client;
+    protected $container;
+    protected $manager;
+
+    public function setUp()
+    {
+        //Load fixtures
+        $fixtures = array(
+            'Mykees\MediaBundle\DataFixtures\ORM\LoadMedias',
+            'Mvc\BlogBundle\DataFixtures\ORM\LoadPostsData',
+        );
+        $this->loadFixtures($fixtures);
+
+        $this->client = static::createClient();
+        $this->container = $this->client->getContainer();
+        $this->manager = $this->container->get('mk.media.manager');
+        $image = $this->client->getKernel()->getRootDir().'/../web/img/';
+
+        parent::setUp();
+    }
+
+    public function testListMedias()
     {
         return true;
     }
