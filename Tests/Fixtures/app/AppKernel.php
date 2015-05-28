784dcae903c5b2a7946f577116ce1b9d1e34917d
Mykees <mykeespro@gmail.com>
2015-05-28 22:07:14 +0200

    update

--- Tests/Fixtures/app/AppKernel.php
+++ Tests/Fixtures/app/AppKernel.php
@@ -3,6 +3,8 @@
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
+require_once __DIR__.'/autoload.php';
+
 class AppKernel extends Kernel
 {
     public function registerBundles()
@@ -25,13 +27,26 @@ class AppKernel extends Kernel
         return $bundles;
     }
 
+    /**
+     * @param LoaderInterface $loader
+     * @return null
+     */
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load($this->getRootDir().'/config/config_test.yml');
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
     }
-
-    public function getRootDir()
+    /**
+     * @return string
+     */
+    public function getCacheDir()
     {
-        return realpath(parent::getRootDir());
+        return sys_get_temp_dir().'/Victoire/cache';
+    }
+    /**
+     * @return string
+     */
+    public function getLogDir()
+    {
+        return sys_get_temp_dir().'/Victoire/logs';
     }
 }
